52bf7096568a1994838a695d99e614680e80b6a1
KajaM12 <kajamoore03@gmail.com>
2025-04-02 01:56:20 +0000

    Added data loading script for books, movies, and music

--- data/load_data.py
+++ data/load_data.py
@@ -4,6 +4,19 @@ def load_books_data():
     df = pd.read_csv("data/books.csv")
     return df
 
+def load_movies_data():
+    df = pd.read_csv("data/movies.csv")
+    return df
+
+def load_music_data():
+    df = pd.read_csv("data/music.csv")
+    return df
+
 if __name__ == "__main__":
     books = load_books_data()
+    movies = load_movies_data()
+    music = load_music_data()
+    
     print(books.head())
+    print(movies.head())
+    print(music.head())
